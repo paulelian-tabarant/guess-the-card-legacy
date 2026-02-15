331b1e6aa756627dd0fd89b49570874d961bca5e
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2026-02-15 15:42:20 +0100

    fix: adapter test d'exemple

--- src/services/GameService.test.ts
+++ src/services/GameService.test.ts
@@ -2,8 +2,7 @@ import {describe, it, expect} from 'vitest'
 
 describe('GameService', () => {
 
-    it('doit afficher "moins" si la carte donnée par l\'utilisateur est en-dessous de celle à deviner', () => {
-        const gameService = new GameServiceTest();
-
+    it('doit afficher "plus" si la carte donnée par l\'utilisateur est en-dessous de celle à deviner', () => {
+        expect(true).toBe(false)
     });
 });
