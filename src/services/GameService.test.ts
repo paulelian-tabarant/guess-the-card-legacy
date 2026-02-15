438f1513fc2e21a9e388d1bfcd722d9cbcd3a291
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2026-02-15 12:10:56 +0100

    test: ajouter dépendances de base pour tester le service

---
+++ src/services/GameService.test.ts
@@ -0,0 +1,8 @@
+import {describe, it, expect} from 'vitest'
+
+describe('GameService', () => {
+
+    it('doit afficher "moins" si la carte donnée par l\'utilisateur est en-dessous de celle à deviner', () => {
+        expect(true).toBe(false)
+    });
+});
